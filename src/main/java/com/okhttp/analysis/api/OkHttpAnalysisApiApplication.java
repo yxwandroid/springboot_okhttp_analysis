4dbe16eafc912a7bbdc7409e0374ee7cfbe4250f
wilson <yxw_android@163.com>
2020-05-15 14:25:37 +0800

    init

---
+++ src/main/java/com/okhttp/analysis/api/OkHttpAnalysisApiApplication.java
@@ -0,0 +1,13 @@
+package com.okhttp.analysis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkHttpAnalysisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkHttpAnalysisApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okhttp/analysis/api/OkhttpAnalysisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okhttp.analysis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkhttpAnalysisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
